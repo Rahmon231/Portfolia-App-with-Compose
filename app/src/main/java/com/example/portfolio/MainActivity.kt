c526669636883fc336c73f7d45dc16d735bdd7ea
Akanbi Rahmon <23046879+Rahmon231@users.noreply.github.com>
2023-06-05 11:54:01 +0100

    app done

--- app/src/main/java/com/example/portfolio/MainActivity.kt
+++ app/src/main/java/com/example/portfolio/MainActivity.kt
@@ -6,17 +6,24 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.portfolio.ui.theme.PortFolioTheme
@@ -39,6 +46,9 @@ class MainActivity : ComponentActivity() {
 }
 @Composable
 fun CreateBizCard(){
+    val rememberButtonState = remember {
+        mutableStateOf(false)
+    }
 Surface(modifier = Modifier
     .fillMaxWidth()
     .fillMaxHeight()) {
@@ -54,6 +64,21 @@ Surface(modifier = Modifier
             CreateImageProfile()
             Divider()
             CreateInfo()
+            Button(
+                onClick = {
+                    rememberButtonState.value = !rememberButtonState.value
+            }
+            ) {
+                Text(text = "Portfolio",
+                style = MaterialTheme.typography.button)
+            }
+            if (rememberButtonState.value ){
+                CreateContent()
+            }else{
+                Box(){
+
+                }
+            }
         }
 
     }
@@ -85,7 +110,7 @@ private fun CreateInfo() {
 @Composable
 private fun CreateImageProfile(modifier: Modifier = Modifier) {
     Surface(
-        modifier = Modifier
+        modifier = modifier
             .size(150.dp)
             .padding(5.dp),
         shape = CircleShape,
@@ -97,10 +122,57 @@ private fun CreateImageProfile(modifier: Modifier = Modifier) {
         Image(
             painter = painterResource(id = R.drawable.profile_pic),
             contentDescription = "profile pic",
-            modifier = Modifier.size(135.dp),
+            modifier = modifier.size(135.dp),
             contentScale = ContentScale.Crop
         )
     }
+}
+
+@Composable
+fun Portfolio(data: List<String>) {
+    LazyColumn{
+        items(data){ item ->
+            Card(modifier = Modifier
+                .fillMaxWidth()
+                .padding(13.dp),
+            shape = RectangleShape,
+            elevation = 4.dp)
+            {
+                Row(modifier = Modifier
+                    .padding(8.dp)
+                    .background(MaterialTheme.colors.surface)
+                    .padding(7.dp)) {
+                    CreateImageProfile(modifier = Modifier.size(100.dp))
+                    Column(modifier = Modifier.padding(7.dp)
+                        .align(alignment = Alignment.CenterVertically)) {
+                        Text(text = item,
+                            fontWeight = FontWeight.Bold)
+                        Text(text = "Project Description",
+                            style = MaterialTheme.typography.body2)
+                    }
+                }
+            }
+        }
+    }
+}
+@Preview
+@Composable
+fun CreateContent(){
+Box(modifier = Modifier
+    .fillMaxWidth()
+    .fillMaxWidth()
+    .padding(5.dp)
+){
+    Surface(modifier = Modifier
+        .fillMaxWidth()
+        .fillMaxHeight()
+        .padding(3.dp),
+        shape = RoundedCornerShape(corner = CornerSize(6.dp)),
+        border = BorderStroke(width = 2.dp, color = Color.LightGray)
+    ) {
+        Portfolio(data = listOf("Project 1", "Project 2", "Project 3"))
+    }
+}
 }
 
 @Preview(showBackground = true)
